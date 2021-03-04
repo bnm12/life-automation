d20e733fbc706b1a1778bfd4dd68f1a144f438dd
Benjamin Kristensen <bnm12@users.noreply.github.com>
2021-03-04 12:52:52 +0100

    Return eerror if there is one

--- src/routes/sleep-as-android.route.ts
+++ src/routes/sleep-as-android.route.ts
@@ -5,38 +5,42 @@ const slack = new WebClient(process.env.SLACK_TOKEN);
 export default {
     method: "post",
     handler: async (request, response) => {
-
-        switch (request.body.event) {
-            case "sleep_tracking_started":
-                await slack.users.setPresence({
-                    "presence": "away"
-                });
-                await slack.users.profile.set({
-                    profile: JSON.stringify({
-                        "status_emoji": ":zzz:",
-                        "status_text": "Taking a nap for 20 min or 3 hours"
-                    })
-                });
-                await slack.dnd.setSnooze({
-                    num_minutes: 60 * 4
-                });
-                break;
-            case "sleep_tracking_stopped":
-                await slack.users.setPresence({
-                    "presence": "auto"
-                });
-                await slack.users.profile.set({
-                    profile: JSON.stringify({
-                        "status_emoji": "",
-                        "status_text": ""
-                    })
-                });
-                await slack.dnd.endDnd();
-                break;
-            default:
-                break;
+        try {
+            switch (request.body.event) {
+                case "sleep_tracking_started":
+                    await slack.users.setPresence({
+                        "presence": "away"
+                    });
+                    await slack.users.profile.set({
+                        profile: JSON.stringify({
+                            "status_emoji": ":zzz:",
+                            "status_text": "Taking a nap for 20 min or 3 hours"
+                        })
+                    });
+                    await slack.dnd.setSnooze({
+                        num_minutes: 60 * 4
+                    });
+                    break;
+                case "sleep_tracking_stopped":
+                    await slack.users.setPresence({
+                        "presence": "auto"
+                    });
+                    await slack.users.profile.set({
+                        profile: JSON.stringify({
+                            "status_emoji": "",
+                            "status_text": ""
+                        })
+                    });
+                    await slack.dnd.endDnd();
+                    break;
+                default:
+                    break;
+            }
+            response.status(200).send(request.body).end() // Responding is important
         }
-        response.status(200).send(request.body).end() // Responding is important
+        catch (error) {
+            response.status(501).send(error).end();
+        }
     },
     path: "/sleep-as-android"
 } as RouteDefinition
